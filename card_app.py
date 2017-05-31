30a8467cd45167b416e218f321b0ae354ab0f9b7
gauderkd <dragnerz@fdraconis.com>
2017-05-31 18:45:04 -0400

    defined a basic user model

--- card_app.py
+++ card_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask.ext.login import login_user , logout_user , current_user , login_required
 from flask.ext.sqlalchemy import SQLAlchemy
 
 # Initialize Flask app
@@ -24,6 +25,19 @@ class Card(db.Model):
     text = db.Column(db.String(500))
     pub_date = db.Column(db.DateTime)
 
+class User(db.Model);
+    __tablename = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(10))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
 
 @app.route('/')
 def hello_world():
