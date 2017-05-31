8744e55992d6e26c152bec540fe09d6fb9360060
gauderkd <dragnerz@fdraconis.com>
2017-05-31 19:33:46 -0400

    set password hash and salt methods

--- card_app.py
+++ card_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, json
-from flask.ext.login import login_user , logout_user , current_user , login_required
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_login import login_user , logout_user , current_user , login_required
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -29,15 +30,22 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column('user_id', db.Integer, primary_key=True)
     username = db.Column('username', db.String(20), unique=True, index=True)
-    password = db.Column('password', db.String(10))
+    password = db.Column('password', db.String(16))
     email = db.Column('email', db.String(50), unique=True, index=True)
     registered_on = db.Column('registered_on', db.DateTime)
 
     def __init__(self, username, password, email):
         self.username = username
-        self.password = password
-        self.email = email
+        self.set_passwrd(password)
+        self.email = email.lower()
         self.registered_on = datetime.utcnow()
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
 
 @app.route('/')
 @app.route('/main')
