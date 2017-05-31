ed6ebcb24336f6da7541311147ed563c25b06ec8
gauderkd <dragnerz@fdraconis.com>
2017-05-31 19:10:49 -0400

    tried to add ajax jquery to signup

--- card_app.py
+++ card_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 from flask.ext.login import login_user , logout_user , current_user , login_required
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -48,3 +48,14 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
+@app.route('/signUp', methods=['POST'])
+def signUp():
+    # Get posted values
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+    # validate
+    if _name and _email and _password:
+        return json.dumps({'html': '<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html': '<span>Enter the required fields</span>'})
