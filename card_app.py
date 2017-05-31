ec3c7c7659e3f00aafbc7dd08f7c861034b9e396
gauderkd <dragnerz@fdraconis.com>
2017-05-31 03:40:54 +0000

    Basid mysql database support

--- card_app.py
+++ card_app.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
+# Initialize Flask app
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+# Connect to database 'carddb'
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="dragnerz",
+    password="cardemia_db",
+    hostname="dragnerz.mysql.pythonanywhere-services.com",
+    databasename="dragnerz$carddb",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+    id = db.Column('card_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String(500))
+    pub_date = db.Column(db.DateTime)
+
 
 @app.route('/')
 def hello_world():
