9eb48d22af0da7254aa5b833ea755b93a98bb4d1
gauderkd <dragnerz@fdraconis.com>
2017-06-03 03:55:48 -0400

    just a temp thing

--- card_app.py
+++ card_app.py
@@ -40,6 +40,36 @@ def user_loader(user_id):
     return Users.query.get(user_id)
 
 
+class Card(db.Model):
+    __tablename__ = 'cards'
+    id = db.Column('card_id', db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    text = db.Column(db.String)
+    pub_date = db.Column(db.DateTime)
+
+
+class Users(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String, unique=True, index=True)
+    password = db.Column('password', db.String)
+    email = db.Column('email', db.String, unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.set_password(password)
+        self.email = email.lower()
+        self.registered_on = datetime.utcnow()
+
+    def set_password(self, plaintext):
+        self.password = argon2.using(rounds=4).hash(plaintext)
+
+    def check_password(self, plaintext):
+        if argon2.verify(plaintext, self.password):
+            return True
+        return False
+
 @app.route('/')
 @app.route('/main')
 def main():
