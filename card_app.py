5fc351e751d1e81e0411a106063d3d5e646593ca
gauderkd <dragnerz@fdraconis.com>
2017-06-03 04:19:28 -0400

    just temps

--- card_app.py
+++ card_app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, flash, session, url_for, redirect
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from passlib.hash import argon2
+from datetime import datetime
 
 from forms import ContactForm, SignupForm, LoginForm
 
@@ -20,8 +24,43 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from models import db, Users
-db.init_app(app)
+db = SQLAlchemy()
+
+
+
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+    id = db.Column('card_id', db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    text = db.Column(db.String)
+    pub_date = db.Column(db.DateTime)
+
+
+class Users(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String, unique=True, index=True)
+    password = db.Column('password', db.String)
+    email = db.Column('email', db.String, unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.set_password(password)
+        self.email = email.lower()
+        self.registered_on = datetime.utcnow()
+
+    def set_password(self, plaintext):
+        self.password = argon2.using(rounds=4).hash(plaintext)
+
+    def check_password(self, plaintext):
+        if argon2.verify(plaintext, self.password):
+            return True
+        return False
+
+
+
 
 # Login Manager
 login_manager = LoginManager()
