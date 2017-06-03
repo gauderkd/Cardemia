510bf962ecfde6889c194bc2ab78c0f9c72c027a
gauderkd <dragnerz@fdraconis.com>
2017-06-03 01:11:06 -0400

    i keep messing up the model name

--- forms.py
+++ forms.py
@@ -23,8 +23,8 @@ class SignupForm(Form):
         if not Form.validate(self):
             return False
 
-        user_mail = User.query.filter_by(email=self.email.data.lower()).first()
-        user_name = User.query.filter_by(username=self.email.data.lower()).first()
+        user_mail = Users.query.filter_by(email=self.email.data.lower()).first()
+        user_name = Users.query.filter_by(username=self.email.data.lower()).first()
         if user_name:
             self.email.errors.append("That username is already taken")
             return False
