62692d79ffc11298570d21c0a10ce3548ce7bb40
gauderkd <dragnerz@fdraconis.com>
2017-06-03 03:03:22 -0400

    changed order

--- card_app.py
+++ card_app.py
@@ -59,11 +59,11 @@ def signin():
 
     if form.validate_on_submit():
         user = Users.query.filter_by(username=form.username.data).first()
-        if user and user.check_password(form.password.data):
+        if user is None:
+            return 'user doesnt exist'
+        elif user and user.check_password(form.password.data):
             login_user(user, remember=True)
             return redirect(url_for('profile'))
-        elif user == None:
-            return 'user doesnt exist'
         else:
             return 'password incorrect'
 
