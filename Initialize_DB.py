54e23df0b8b0591f51749cb305b31c44a5b98308
gauderkd <dragnerz@fdraconis.com>
2017-06-06 04:10:11 -0400

    trying to work out Cards database

--- Initialize_DB.py
+++ Initialize_DB.py
@@ -25,9 +25,49 @@ db = SQLAlchemy(app)
 class Card(db.Model):
     __tablename__ = 'cards'
     id = db.Column('card_id', db.Integer, primary_key=True)
+
+    owner = db.Column(db.String)
+
     title = db.Column(db.String)
-    text = db.Column(db.String)
-    pub_date = db.Column(db.DateTime)
+    year = db.Column(db.String)
+    authors = db.Column(db.String)
+
+    keywords = db.Column(db.String)
+
+    sec_1 = db.Column(db.String)
+    sec_2 = db.Column(db.String)
+    sec_3 = db.Column(db.String)
+    sec_4 = db.Column(db.String)
+    sec_5 = db.Column(db.String)
+    sec_6 = db.Column(db.String)
+    sec_7 = db.Column(db.String)
+    sec_8 = db.Column(db.String)
+    sec_9 = db.Column(db.String)
+    sec_10 = db.Column(db.String)
+
+    registered_on = db.Column(db.DateTime)
+    last_edited = db.Column(db.DateTime)
+
+    def __init__(self, title, year, authors, keywords="", sec_1="", sec_2="", sec_3="", sec_4="",
+                 sec_5="", sec_6="",sec_7="", sec_8="", sec_9="", sec_10=""):
+        self.title = title
+        self.year = year
+        self.authors = authors
+        self.keywords = keywords
+        self.sec_1 = sec_1
+        self.sec_2 = sec_2
+        self.sec_3 = sec_3
+        self.sec_4 = sec_4
+        self.sec_5 = sec_5
+        self.sec_6 = sec_6
+        self.sec_7 = sec_7
+        self.sec_8 = sec_8
+        self.sec_9 = sec_9
+        self.sec_10 = sec_10
+        self.registered_on = datetime.utcnow()
+
+
+
 
 class Users(db.Model, UserMixin):
     __tablename__ = "users"
