591387f9327d5c66cb1bbab9c45ac1e4049d4303
gauderkd <dragnerz@fdraconis.com>
2017-06-08 00:07:47 -0400

    edit?

--- card_app.py
+++ card_app.py
@@ -152,7 +152,7 @@ def viewcard(variable):
         form = ViewEditCard()
         this_card = Card.query.filter(Card.id == variable, Card.owner == current_user).first()
         if request.method == 'POST':
-            this_card.edit_text("TEST STRING")
+            this_card.card_text = "TEST"
         return render_template("viewcard.html", card=this_card, form=form)
     else:
         flash('Please sign in to make and view cards')
