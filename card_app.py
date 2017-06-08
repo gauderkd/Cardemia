c6f2ca5139aa5f01228c144d4795562e651b3e3c
gauderkd <dragnerz@fdraconis.com>
2017-06-08 00:05:00 -0400

    this will be messy

--- card_app.py
+++ card_app.py
@@ -152,8 +152,7 @@ def viewcard(variable):
         form = ViewEditCard()
         this_card = Card.query.filter(Card.id == variable, Card.owner == current_user).first()
         if request.method == 'POST':
-            if this_card.card_text is not form.text.data:
-                this_card.edit_text(form.text.data)
+            this_card.edit_text(form.text.data)
         return render_template("viewcard.html", card=this_card, form=form)
     else:
         flash('Please sign in to make and view cards')
